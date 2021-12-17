4f625407118ac863df7ca3cc46057ac8c67a4817
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2021-12-16 22:55:42 -0600

    Finished setup, wrote initial html tags

---
+++ server/public/script/client.js
@@ -0,0 +1,17 @@
+console.log('js');
+
+$(document).ready(handleReady);
+
+function handleReady() {
+    console.log('jQuery is ready');
+    }
+
+$().on('click')
+
+
+/* {
+    numOne: numOne,
+    operator: operator,
+    numTwo; numTwo,
+    solution: 0
+} */
